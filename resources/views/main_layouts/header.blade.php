14506ccc36a507bb1a617c50a18e335e6bd7a37f
y2jsheikh <y2jsheikh@hotmail.com>
2022-06-17 10:59:56 +0500

    Header link updated

--- resources/views/main_layouts/header.blade.php
+++ resources/views/main_layouts/header.blade.php
@@ -30,12 +30,12 @@
             <li ><a href="#events">Events</a></li>
             <li class="has-dropdown"><a href="#">NAP</a>
                 <ul class="dropdown">
-                    <li><a href="#">The Context</a></li>
-                    <li><a href="#">Vision</a></li>
+                    {{--<li><a href="#">The Context</a></li>--}}
+                    {{--<li><a href="#">Vision</a></li>--}}
                     <li><a href="{{url("cci_recommendation")}}">CCI Recommendations</a></li>
                     <li><a href="{{url("https://visualindicators.com/cciportal/login")}}">CCI Portal</a></li>
                     <li><a href="{{url("task_force")}}">Task Force</a></li>
-                    <li><a href="#">Meetings FTF</a></li>
+                    {{--<li><a href="#">Meetings FTF</a></li>--}}
                     <li><a href="{{url("national_action_plan")}}">NAP Progress</a></li>
                     <li><a href="{{url("pakistan_population_fund")}}">Population Fund</a></li>
                 </ul>
@@ -43,8 +43,8 @@
             <li class="has-dropdown"><a href="#">Media Centre</a>
                 <ul class="dropdown">
                     <li><a href="{{url('media')}}">Press Releases</a></li>
-                    <li><a href="{{url('media')}}">Social Media Links</a></li>
-                    <li><a href="{{url('media')}}">News Letter</a></li>
+                    <li><a href="#">Training Material</a></li>
+                    <li><a href="#">Publication</a></li>
                     <li><a href="{{url('media')}}">Picture Gallery</a></li>
                     <li><a href="{{url('videos')}}">Videos</a></li>
                 </ul>
