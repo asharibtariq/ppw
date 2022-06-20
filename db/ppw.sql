f156203055489a2c2617b8f8c27daec0d8b2f4e9
y2jsheikh <y2jsheikh@hotmail.com>
2022-06-20 09:53:11 +0500

    D Added

--- db/ppw.sql
+++ db/ppw.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50724
 File Encoding         : 65001
 
-Date: 2022-06-14 10:08:30
+Date: 2022-06-20 09:52:41
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -44,7 +44,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
 -- Records of migrations
@@ -54,6 +54,11 @@ INSERT INTO `migrations` VALUES ('2', '2014_10_12_100000_create_password_resets_
 INSERT INTO `migrations` VALUES ('3', '2019_08_19_000000_create_failed_jobs_table', '1');
 INSERT INTO `migrations` VALUES ('4', '2019_12_14_000001_create_personal_access_tokens_table', '1');
 INSERT INTO `migrations` VALUES ('5', '2022_06_14_033503_add_username', '2');
+INSERT INTO `migrations` VALUES ('6', '2022_06_14_054909_create_news_table', '3');
+INSERT INTO `migrations` VALUES ('7', '2022_06_14_091157_create_events_table', '4');
+INSERT INTO `migrations` VALUES ('8', '2022_06_14_104242_create_contacts_table', '5');
+INSERT INTO `migrations` VALUES ('9', '2022_06_15_081835_create_teams_table', '6');
+INSERT INTO `migrations` VALUES ('10', '2022_06_17_061436_create_publications_table', '7');
 
 -- ----------------------------
 -- Table structure for password_resets
@@ -91,6 +96,131 @@ CREATE TABLE `personal_access_tokens` (
 
 -- ----------------------------
 -- Records of personal_access_tokens
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tbl_contact
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_contact`;
+CREATE TABLE `tbl_contact` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lastname` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phonenumber` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `message` text COLLATE utf8mb4_unicode_ci,
+  `status` enum('Y','N') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Y',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of tbl_contact
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tbl_event
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_event`;
+CREATE TABLE `tbl_event` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `author` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `location` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `date` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` enum('Y','N') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Y',
+  `created_by` int(11) NOT NULL DEFAULT '0',
+  `updated_by` int(11) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of tbl_event
+-- ----------------------------
+INSERT INTO `tbl_event` VALUES ('1', 'Lure Irure', 'Council of Islamic Ideology arranged an Ulema Conference', 'The conference was attended by a score of renowned religious scholars.\r\n                            The Minister of National Health Services was the Chief Guest at the Conference.', 'event1.jpg', '2736 Hinkle Deegan Lake Road', '06-Mar-1976', 'Y', '1', '1', '2022-06-14 10:08:14', '2022-06-14 10:08:14');
+INSERT INTO `tbl_event` VALUES ('2', 'Lure Irure', 'Population Wing arranged a Training session', 'opulation Wing arranged a Training session for Public Private partnership of Doctors and Nurses on Post Partum Contraceptive use in ICT. DG met all participants and a healthy discussion on way forward.', 'event2.jpg', '2736 Hinkle Deegan Lake Road', '06-Mar-1976', 'Y', '1', '1', '2022-06-14 10:08:14', '2022-06-14 10:08:14');
+INSERT INTO `tbl_event` VALUES ('3', 'Lure Irure', 'MPDSR National Meeting', 'The event was graced by Iftikhar Shallwani Special Secretary M/o MHSRC@Shallwani DG Sabina Durrani PPW, Dr Mohamed Afifi Regional Director, and health professionals from all provinces. Maternal mortality and ways to curtail its occurance was discussed.', 'event3.jpg', '2736 Hinkle Deegan Lake Road', '06-Mar-1976', 'Y', '1', '1', '2022-06-14 10:08:14', '2022-06-14 10:08:14');
+INSERT INTO `tbl_event` VALUES ('4', 'Lure Irure', 'Health Services Academy & Accountability Group meeting', 'Health Services Academy arranged a Technical Advisory & Accountability Group meeting On #MNCH and #FP. It was attended by DG PPW and other health Officials including Dr Jamil.', 'event4.jpg', '2736 Hinkle Deegan Lake Road', '06-Mar-1976', 'Y', '1', '1', '2022-06-14 10:08:14', '2022-06-14 10:08:14');
+
+-- ----------------------------
+-- Table structure for tbl_news
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_news`;
+CREATE TABLE `tbl_news` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
+  `author` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` enum('Y','N') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Y',
+  `created_by` int(11) NOT NULL DEFAULT '0',
+  `updated_by` int(11) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of tbl_news
+-- ----------------------------
+INSERT INTO `tbl_news` VALUES ('1', 'Special Secretary Iftikhar Ali Shallwani\r\n                                called a meeting at the PPW Resource Centre', 'Meeting was held to discuss the current work done on Population. He called on the wing to get to the ground level of\r\n                            efforts by all stakeholders and gave some recommendations for a way forward @UNFPA.', 'Velit Nazu', 'news4.jpg', 'Y', '1', '1', '2022-06-14 07:26:14', '2022-06-14 07:26:14');
+INSERT INTO `tbl_news` VALUES ('2', 'Tahir Mehmood Ashrafi ( All Pakistan Ulema Council of Pakistan) met with DG PPW', 'In the Population Program Wing of MNHSRC. Matters pertaining to the State and its policies regarding Family Planning were discussed. Tahir Mehmood Ashrafi said that Behbud-eAbadi important.', 'Velit Nazu', 'news7.jpg', 'Y', '1', '1', '2022-06-14 07:26:14', '2022-06-14 07:26:14');
+INSERT INTO `tbl_news` VALUES ('3', 'Ministry of NHSRC planned a meeting under the chairmanship of DG', 'With the stakeholders of the Medical Tourism Initiative, to discuss a way forward. Focal persons from the\r\n                            Ministry of Interior and Foreign Affairs along with the PTDC Director gave reports on the initiative.', 'Velit Nazu', 'news3.jpg', 'Y', '1', '1', '2022-06-14 07:26:14', '2022-06-14 07:26:14');
+INSERT INTO `tbl_news` VALUES ('4', 'DG PPW met with Dr Mohammed Afifi', 'Regional Advisor on Women Health. Ms Ellen Thom, Team Lead for Healthier Population through the Life Course and\r\n                            Dr Qudsia Uzma, Technical Officer', 'Velit Nazu', 'news1.jpg', 'Y', '1', '1', '2022-06-14 07:26:14', '2022-06-14 07:26:14');
+INSERT INTO `tbl_news` VALUES ('5', 'Meeting with Dr. Qibla Ayaz (CII)', 'DG Population Program Wing and team met with Dr. Qibla Ayaz (CII) for guidance and cooperation on holding Ulema conference.', 'Velit Nazu', 'news5.jpg', 'Y', '1', '1', '2022-06-14 07:26:14', '2022-06-14 07:26:14');
+INSERT INTO `tbl_news` VALUES ('6', 'A meeting with PPW partners from HSA UNFPA', 'Where a presentation was given on Midwifery in Pakistan and its plans of way forward. DG Dr. Sabina Durrani gave her suggestions and concerns.', 'Velit Nazu', 'news6.jpg', 'Y', '1', '1', '2022-06-14 07:26:14', '2022-06-14 07:26:14');
+
+-- ----------------------------
+-- Table structure for tbl_publication
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_publication`;
+CREATE TABLE `tbl_publication` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
+  `author` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `document` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` enum('Y','N') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Y',
+  `created_by` int(11) NOT NULL DEFAULT '0',
+  `updated_by` int(11) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of tbl_publication
+-- ----------------------------
+INSERT INTO `tbl_publication` VALUES ('1', 'Comparison of need and supply of syringes for therapeutic injections in Pakistan', 'Adnan Ahmad Khan, Momina Saleem, Huma Qureshi, Rashid Jooma, Ayesha Khan To determine the extent of shortfall in syringes by measuring the need from nationally validated sources and comparing it with position on the supply side. This was done in order to contribute to the ongoing national discussions that have focused on increasing syringe supply to curtail syringe reuse.', 'Abc', 'TEMP-PDF-Document.pdf', 'Y', '1', '1', '2022-06-17 06:40:43', '2022-06-17 09:02:15');
+INSERT INTO `tbl_publication` VALUES ('2', 'Costs and Utilization of Public Sector Family Planning Services in Pakistan', 'Khadija Abbas, M.Sc., Adnan Ahmad Khan, MBBS, MS, Ayesha Khan, MBBS, MPH The public sector provides a third of family planning (FP) services in Pakistan. However, these services are viewed as being underutilized and expensive. We explored the utilization patterns and costs of FP services in the public sector.', 'someone', 'TEMP-PDF-Document.pdf', 'Y', '1', '1', '2022-06-17 06:45:52', '2022-06-17 07:15:05');
+INSERT INTO `tbl_publication` VALUES ('3', 'Costs and Utilization of Public Sector Family Planning Services in Pakistan', 'Khadija Abbas, M.Sc., Adnan Ahmad Khan, MBBS, MS, Ayesha Khan, MBBS, MPH The public sector provides a third of family planning (FP) services in Pakistan. However, these services are viewed as being underutilized and expensive. We explored the utilization patterns and costs of FP services in the public sector.', 'aaa', 'TEMP-PDF-Document.pdf', 'Y', '1', '1', '2022-06-17 09:05:07', '2022-06-17 09:05:07');
+
+-- ----------------------------
+-- Table structure for tbl_team
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_team`;
+CREATE TABLE `tbl_team` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `designation` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `department` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
+  `status` enum('Y','N') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Y',
+  `created_by` int(11) NOT NULL DEFAULT '0',
+  `updated_by` int(11) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of tbl_team
 -- ----------------------------
 
 -- ----------------------------
